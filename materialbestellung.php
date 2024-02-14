167c30c7ab40738704f1abc7d6525060eca5e047
Marc Michalsky <michalsky@forumzfd.de>
2024-02-14 12:46:00 +0100

    civix upgrade

--- materialbestellung.php
+++ materialbestellung.php
@@ -40,15 +40,6 @@ function materialbestellung_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function materialbestellung_civicrm_xmlMenu(&$files) {
-  _materialbestellung_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -107,54 +98,6 @@ function materialbestellung_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) 
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function materialbestellung_civicrm_managed(&$entities) {
-  _materialbestellung_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function materialbestellung_civicrm_caseTypes(&$caseTypes) {
-  _materialbestellung_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function materialbestellung_civicrm_angularModules(&$angularModules) {
-  _materialbestellung_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function materialbestellung_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _materialbestellung_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Function to check if the required extensions are installed
  *
  * @throws Exception
@@ -187,9 +130,8 @@ function _materialbestellung_required_extensions_installed() {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function materialbestellung_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
@@ -207,3 +149,12 @@ function materialbestellung_civicrm_navigationMenu(&$menu) {
   ));
   _materialbestellung_civix_navigationMenu($menu);
 } // */
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function materialbestellung_civicrm_entityTypes(&$entityTypes) {
+  _materialbestellung_civix_civicrm_entityTypes($entityTypes);
+}
